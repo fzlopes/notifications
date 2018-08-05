ae040432d8c0c86eb6398641b2f1f63ef6dce0e5
Fábio Lopes <fzlopes1@gmail.com>
2018-08-05 03:44:09 -0300

    Criação da model Comment, migration comments e seus relacionamentos.

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Posts extends Model
 {
-    //
+	protected $fillable = ['user_id', 'title', 'body']
+
+    public function comments()
+    {
+    	return $this->hasMany(Comment::class);
+    }
+
+    //Autor post
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
 }
